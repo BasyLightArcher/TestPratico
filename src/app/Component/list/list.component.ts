e42edd4f58793eba8bdaa998ba44c7a7ca56b0e5
uenz98 <marco.penzo98@gmail.com>
2019-04-03 16:03:15 +0200

    list creation with some items

--- src/app/Component/list/list.component.ts
+++ src/app/Component/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Sweet } from 'src/app/classes/SweetsInterface';
+import { ListService } from 'src/app/service/list.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private listService:ListService) { }
 
   ngOnInit() {
   }
-
+  sweets:Sweet[] = this.listService.getList();
 }
