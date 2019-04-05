67294ad2daf07f4fa1cc91d83e8d757d1c5c9468
Basile Ikokwu <48047915+BasyLightArcher@users.noreply.github.com>
2019-04-05 16:58:09 +0200

    fix

--- src/app/service/list.service.ts
+++ src/app/service/list.service.ts
@@ -5,7 +5,7 @@ import {Sweet} from 'src/app/classes/SweetsInterface';
 })
 export class ListService {
   sweets:Sweet[] = [
-    {id:1,name:'Torta alle mele',tipologia:'Torta',descrizione:''},
+    {id:1,name:'Torta alle mele',tipologia:'Torta',descrizione:'torta alle mele'},
     {id:2,name:'Cheesecake alla fragola',tipologia:'Torta',descrizione:''},
     {id:3,name:'Creme caramel',tipologia:'Al cucchiaio',descrizione:''},
     {id:4,name:'Cannoli siciliani',tipologia:'Piccola pasticceria',descrizione:''},
@@ -14,7 +14,7 @@ export class ListService {
     {id:7,name:'Gelato al cioccolato',tipologia:'Classici',descrizione:''},
     {id:8,name:'Crèpe alla nutella',tipologia:'Salati',descrizione:''},
     {id:9,name:'Babà',tipologia:'Piccola pasticceria',descrizione:''},
-    {id:10,name:'Torta margherita allo yogurt',tipologia:'Torta',descrizione:''}
+    {id:10,name:'Torta margherita allo yogurt',tipologia:'Torta',descrizione:'versione light della torta margherita'}
   ];
   constructor() { }
 
