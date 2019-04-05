3df180abf509f75dc441c57266969ed46fc5097b
uenz98 <marco.penzo98@gmail.com>
2019-04-05 14:54:52 +0200

    added colors in variables and changed global background gradient in syles.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
   title = 'TestPratico';
+  constructor(private elementRef:ElementRef){}
+  ngAfterViewInit(){
+    this.elementRef.nativeElement.ownerDocument.body.style.background = 'linear-gradient(180deg, #00357F, #00357F, 100%)';
+  }
 }
