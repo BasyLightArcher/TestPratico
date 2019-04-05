4093232e1c494274eeaeb7b9c28ec96af81ced71
ludovico.lalli <ludovico.lalli@gmail.com>
2019-04-05 16:44:40 +0200

    cambio cartella file register in Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RoutingEnum } from './classes/RoutingEnum';
 import { LoginComponent } from './Component/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponent } from './Component/register/register.component';
+import { HomepageComponent } from './Component/homepage/homepage.component';
 
 const routes: Routes = [
   {path: RoutingEnum.login, component: LoginComponent},
   {path: RoutingEnum.register, component: RegisterComponent},
+  {path: RoutingEnum.home, component: HomepageComponent},
   {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
 
