a734971d4292fa2d4a67fc8f84c023a40c3d43e5
ludovico.lalli <ludovico.lalli@gmail.com>
2019-04-05 16:40:11 +0200

    cambio cartella register nell'app-routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RoutingEnum } from './classes/RoutingEnum';
 import { LoginComponent } from './Component/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponent } from './Component/register/register.component';
 import { HomepageComponent } from './Component/homepage/homepage.component';
 import { ListComponent } from './Component/list/list.component';
 
