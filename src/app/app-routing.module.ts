59590348daf6fb69366d91ee51222dcac89e16b4
ludovico.lalli <ludovico.lalli@gmail.com>
2019-04-05 15:29:34 +0200

    modifica routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { RoutingEnum } from './classes/RoutingEnum';
 import { LoginComponent } from './Component/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomepageComponent } from './Component/homepage/homepage.component';
+import { ListComponent } from './Component/list/list.component';
 
 const routes: Routes = [
   {path: RoutingEnum.login, component: LoginComponent},
   {path: RoutingEnum.register, component: RegisterComponent},
+  {path: RoutingEnum.home, component: HomepageComponent},
+  {path: RoutingEnum.list, component: ListComponent},
   {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
 
