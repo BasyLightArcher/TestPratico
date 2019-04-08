c80770ac89fae063f4901b3a806563a61a38e006
uenz98 <marco.penzo98@gmail.com>
2019-04-08 10:38:24 +0200

    updated register.scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RoutingEnum } from './classes/RoutingEnum';
 import { LoginComponent } from './Component/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponent } from './component/register/register.component';
 
 const routes: Routes = [
   {path: RoutingEnum.login, component: LoginComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponent } from './component/register/register.component';
 import { MenuComponent } from './Component/menu/menu.component';
 import { ListComponent } from './Component/list/list.component';
 import {FormsModule} from '@angular/forms'
