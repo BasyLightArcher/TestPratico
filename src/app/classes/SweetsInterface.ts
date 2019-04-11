7554a071740d4366f7914d89b6bb5b24600fb5d6
uenz98 <marco.penzo98@gmail.com>
2019-04-11 11:57:28 +0200

    list menu added

--- src/app/classes/SweetsInterface.ts
+++ src/app/classes/SweetsInterface.ts
@@ -3,4 +3,7 @@ export interface Sweet{
     name:string;
     tipologia:string; //torte, al cucchiaio, piccola pasticceria, classici
     descrizione:string;
+    favorite:boolean;
+    showMenu:boolean;
+    hidden:boolean;
 }
